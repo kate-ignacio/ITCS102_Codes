46f5df02e1394859cbcd80859a5c2388cc35cece
kate-ignacio <kateignacio02@gmail.com>
2026-09-03 11:21:40 +0800

    still not done

---
+++ code_challenge2.py
@@ -0,0 +1,22 @@
+#peso denominator
+#money = 4572
+# 1000, 500, 200, 100, 50, 20, 10, 5, 1
+
+libo_sukli = money % 1000 # 572
+#print(libo_sukli)
+#libo_sukli = money - (libo * 1000)
+
+fh = libo_sukli // 500 #50
+fh_ch = money % 500 # 72
+#print(fh_ch)
+#fh_ch = money - (fh * 500)
+
+fif = fh_ch // 50 #22
+fif_c = money % 50
+#print(fif_c)
+#fif_ch = money - (fif * 50)
+
+
+
+
+
