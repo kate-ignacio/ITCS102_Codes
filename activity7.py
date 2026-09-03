52e3fea8179a449438e7b919ab8e94f1a4155838
kate-ignacio <kateignacio02@gmail.com>
2026-09-03 10:58:51 +0800

    activities that need to pass

---
+++ activity7.py
@@ -0,0 +1,25 @@
+#assignment operators
+total = 0 
+n1= eval(input("Input a number -- "))
+total = total + n1
+n2= eval(input("Input a number -- "))
+total += n2
+n3= eval(input("Input a number -- "))
+total += n3
+n4= eval(input("Input a number -- "))
+total += n4
+n5= eval(input("Input a number -- "))
+total *= n5
+n6= eval(input("Input a number -- "))
+total *= n6
+n7= eval(input("Input a number -- "))
+total -= n7
+n8= eval(input("Input a number -- "))
+total -= n8
+n9= eval(input("Input a number -- "))
+total /= n9
+n10= eval(input("Input a number -- "))
+total /= n10
+
+
+print("total is -- ", total)
