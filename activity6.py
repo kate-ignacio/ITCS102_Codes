78ec2d78e73b9170b0a29163e9a7f321dbbf55ba
kate-ignacio <kateignacio02@gmail.com>
2026-09-03 10:59:57 +0800

    activities that need to be submitted

---
+++ activity6.py
@@ -0,0 +1,16 @@
+#int(), or eval (), float ()
+x = int(input("What is you name? "))
+
+sum = x + 5
+
+print("Total = ", sum)
+
+print(type(x))
+
+x = int(input("What is you name? "))
+
+sum = x + 5.6
+
+print("Total = ", sum)
+
+print(type(x))
