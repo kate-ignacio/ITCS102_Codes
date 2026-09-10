95c6cba8b68cd4f6f37dfbbe0b3f5cdcfff07a29
kate-ignacio <kateignacio02@gmail.com>
2026-09-10 19:47:10 +0800

    Code_Challenge3_to_be_sumbitted
    Refactored code to calculate currency denominations based on user input instead of hardcoded values.

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,22 +1,38 @@
-#peso denominator
-#money = 4572
-# 1000, 500, 200, 100, 50, 20, 10, 5, 1
+# Get amount from user
+amount = int(input("Enter amount of money: "))
+original = amount
 
-libo_sukli = money % 1000 # 572
-#print(libo_sukli)
-#libo_sukli = money - (libo * 1000)
+# Denominations — largest to smallest, separated 1000 and 500
+p1000 = amount // 1000
+amount = amount % 1000
 
-fh = libo_sukli // 500 #50
-fh_ch = money % 500 # 72
-#print(fh_ch)
-#fh_ch = money - (fh * 500)
+p500 = amount // 500
+amount = amount % 500
 
-fif = fh_ch // 50 #22
-fif_c = money % 50
-#print(fif_c)
-#fif_ch = money - (fif * 50)
+p200 = amount // 200
+amount = amount % 200
+
+p150 = amount // 150
+amount = amount % 150
+
+p20 = amount // 20
+amount = amount % 20
+
+p10 = amount // 10
+amount = amount % 10
+
+p5 = amount // 5
+amount = amount % 5
+
+p1 = amount // 1
 
 
-
-
-
+print("Amount Entered:", original)
+print("1000 :", p1000)
+print("500  :", p500)
+print("200  :", p200)
+print("150  :", p150)
+print("20   :", p20)
+print("10   :", p10)
+print("5    :", p5)
+print("1    :", p1)
